f20add5bdfc5a176072950585c20e99cc534ebc4
User <yuruyu6@gmail.com>
2020-08-26 20:08:24 +0300

    Added cyrillic support

--- scripts.js
+++ scripts.js
@@ -10,6 +10,53 @@ function getRecordsValue(value) {
   document.getElementById("recordsNumber").value = value;
 }
 
+function useTransliteration(str) {
+  const ru = new Map([
+    ["а", "a"],
+    ["б", "b"],
+    ["в", "v"],
+    ["г", "g"],
+    ["д", "d"],
+    ["е", "e"],
+    ["є", "e"],
+    ["ё", "e"],
+    ["ж", "j"],
+    ["з", "z"],
+    ["и", "i"],
+    ["ї", "yi"],
+    ["й", "i"],
+    ["к", "k"],
+    ["л", "l"],
+    ["м", "m"],
+    ["н", "n"],
+    ["о", "o"],
+    ["п", "p"],
+    ["р", "r"],
+    ["с", "s"],
+    ["т", "t"],
+    ["у", "u"],
+    ["ф", "f"],
+    ["х", "h"],
+    ["ц", "c"],
+    ["ч", "ch"],
+    ["ш", "sh"],
+    ["щ", "shch"],
+    ["ы", "y"],
+    ["э", "e"],
+    ["ю", "u"],
+    ["я", "ya"],
+  ]);
+  str = str.replace(/[ъь]+/g, "");
+  return Array.from(str).reduce(
+    (s, l) =>
+      s +
+      (ru.get(l) ||
+        (ru.get(l.toLowerCase()) === undefined && l) ||
+        ru.get(l.toLowerCase()).toUpperCase()),
+    ""
+  );
+}
+
 function doMainAction() {
   let resultString = "";
   createStringArray().forEach((element) => {
@@ -23,6 +70,12 @@ function createStringArray() {
   let dataString = document.getElementById("dataString").value;
   var stringArray = [];
   var newString = "";
+  const cyrillicPattern = /[\u0400-\u04FF]/;
+  
+  if (cyrillicPattern.test(dataString)) {
+    dataString = useTransliteration(dataString);
+  }
+
   for (var i = 0; i < document.getElementById("recordsNumber").value; i++) {
     for (var j = 0; j < dataString.length; j++) {
       if (document.getElementById("useLetterChanger").checked) {
@@ -116,8 +169,8 @@ let letterDict0 = [
   ["K", "|<"],
   ["k", "|<"],
   ["V", "\\/"],
-  ["Y", "РЈ"],
-  ["y", "РЈ"],
+  ["Y", "Ј"],
+  ["y", "j"],
   ["W", "VV"],
   ["Y", "`/"],
   ["X", "}{"],
@@ -154,11 +207,10 @@ let letterDict1 = [
   ["K", "Q"],
   ["k", "q"],
   ["V", "W"],
-  ["Y", "У"],
+  ["Y", "J"],
   ["y", "у"],
   ["W", "VV"],
   ["w", "vv"],
-  ["Y", "U"],
   ["X", "Х"],
   ["U", "J"],
   ["I", "l"],
